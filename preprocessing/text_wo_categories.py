501dd796f48023e6e5b2c8ac682a70af118d779a
Umesh Singla <umeshksingla@gmail.com>
2017-10-26 17:59:35 +0530

    Separate folders for each step

--- preprocessing/text_wo_categories.py
+++ preprocessing/text_wo_categories.py
@@ -2,6 +2,9 @@
 
 '''
 File desc: Get text where category or categories is not given
+
+NOTE: You can change code by removing 'not' if you want the
+text where category is given.
 '''
 
 import json
@@ -11,7 +14,7 @@ en_articles = {}
 
 c = 0
 for i in range(1, 10):
-	file = '../IRE_project_snapshot/0' + str(i) + '-05-2017_2'
+	file = '../../IRE_project_snapshot/0' + str(i) + '-05-2017_2'
 	#print(file)
 	with open(file, 'r') as f:
 		for line in f:
@@ -30,7 +33,9 @@ for i in range(1, 10):
 				pass
 				#print("null")
 
-for k, v in en_articles.items():
-	if "category" not in v.keys():
-		print(k, end=" ")
-		print(v["desc"])
+o = {}
+with open('articles_wo.json', 'w') as w:
+	for k, v in en_articles.items():
+		if "category" not in v.keys():
+			o = {"id": k, "desc": v["desc"]}
+			w.write(json.dumps(o) + '\n')
