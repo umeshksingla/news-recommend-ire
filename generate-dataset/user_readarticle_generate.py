a1471d3100d3f2c759b199e6cbf128a1e3792bdc
Umesh Singla <umeshksingla@gmail.com>
2017-11-10 02:43:52 +0530

    Remove 'rating' = 10 column
    Rating column is no longer needed and carries no meaning in our
    context. Also, only the read articles have been considered. Negative
    Sampling is not part of the project.

--- generate-dataset/user_readarticle_generate.py
+++ generate-dataset/user_readarticle_generate.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 '''
-File desc: <user_id> <article_id> <read/not-read> format from <date>_filtered_user_en_articles.json
+File desc: <user_id> <article_id> format from <date>_filtered_user_en_articles.json
 '''
 
 import json
@@ -9,7 +9,7 @@ import json
 user_articles = {}
 
 for i in range(1, 10):
-	file = '0' + str(i) + '-05-2017_filtered_user_en_articles.json'
+	file = '../../json_files_english_articles_users/0' + str(i) + '-05-2017_filtered_user_en_articles.json'
 	print(file)
 	with open(file, 'r') as f:
 		for line in f:
@@ -22,6 +22,7 @@ for i in range(1, 10):
 						user_articles[user_id].add(a_id)
 
 with open('user_article_read.csv', 'w') as w:
+	w.write("user_id" + ',' + "article_id" + '\n')
 	for user_id, articles in user_articles.items():
 		for a_id in articles:
-			w.write(user_id + ',' + a_id + ',' + '10' + '\n')
+			w.write(user_id + ',' + a_id + '\n')
